c0ed04dd648e09a23ec35885a23422eb28938bf8
Asad Hameed <asadhameed2002@yahoo.com>
2020-10-17 19:55:40 +0200

    Add winston package for log

--- server/server.js
+++ server/server.js
@@ -4,16 +4,23 @@ import "express-async-errors";
 import winston from "winston";
 import config from "../config";
 
+winston.exceptions.handle(
+  new winston.transports.Console({ colorize: true, prettyPrint: true }),
+  new winston.transports.File({ filename: "exceptions.log" })
+);
+
+process.on("unhandledRejection", (ex) => {
+  throw ex;
+});
+
+winston.add(new winston.transports.File({ filename: "logs.log" }));
 mongoose
   .connect(config.mongoUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
-  .then(() => console.log(`Connect with ${config.mongoUri}`))
-  .catch((err) =>
-    console.log(` Unable to connect with ${config.mongoUri} and error `, err)
-  );
+  .then(() => console.log(`Connect with ${config.mongoUri}`));
 app.listen(config.port, function onStart(err) {
   if (err) console.log(err);
   console.info("Server start on port %s", config.port);
